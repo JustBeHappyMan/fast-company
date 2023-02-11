c6dd48ac93c6c8436b3ca90115edc327e5429589
TheConstantinus <konstantin-nt@bk.ru>
2023-02-12 02:27:27 +0500

    Вынести состояния и методы в компонент App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Users from "./components/users";
+import SearchStatus from "./components/searchStatus";
+import api from "./api/api";
 
 function App() {
-  return <Users />;
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const handleDelete = (id) => {
+    setUsers(users.filter((user) => user._id !== id));
+  };
+
+  const handleToggleBookmark = (id) => {
+    setUsers(
+      users.map((user) => {
+        user._id === id && (user.bookmark = !user.bookmark);
+
+        return user;
+      })
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <SearchStatus length={users.length} />
+      <Users
+        users={users}
+        handleDelete={handleDelete}
+        handleToggleBookmark={handleToggleBookmark}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,55 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import User from "./user";
-import api from "../api/api";
 
-import SearchStatus from "./searchStatus";
-
-const Users = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
-
-  const handleDelete = (id) => {
-    setUsers(users.filter((user) => user._id !== id));
-  };
-
-  const handleToggleBookmark = (id) => {
-    setUsers(
-      users.map((user) => {
-        user._id === id && (user.bookmark = !user.bookmark);
-
-        return user;
-      })
-    );
-  };
-
+const Users = ({ users, ...rest }) => {
   return (
-    <React.Fragment>
-      <SearchStatus length={users.length} />
-      {users.length > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <User
-                key={user._id}
-                handleDelete={handleDelete}
-                handleToggleBookmark={handleToggleBookmark}
-                {...user}
-              />
-            ))}
-          </tbody>
-        </table>
-      )}
-    </React.Fragment>
+    users.length > 0 && (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col">Избранное</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <User
+              key={user._id}
+              handleDelete={rest.handleDelete}
+              handleToggleBookmark={rest.handleToggleBookmark}
+              {...user}
+            />
+          ))}
+        </tbody>
+      </table>
+    )
   );
 };
 
